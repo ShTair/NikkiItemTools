8e3827f625aa192fff8015148c52897800bdd411
ShTair <git@vsh.jp>
2017-06-03 00:41:10 +0900

    リファクタリングと、倍率を考慮

--- NikkiItem.Models/Item.cs
+++ NikkiItem.Models/Item.cs
@@ -1,4 +1,5 @@
 ﻿using CsvHelper.Configuration;
+using System;
 
 namespace NikkiItem.Models
 {
@@ -66,6 +67,118 @@ namespace NikkiItem.Models
                 Map(m => m.Tags).Index(16);
                 Map(m => m.Color).Index(17);
             }
+        }
+
+        public static int GetKindId(string kind)
+        {
+            switch (kind)
+            {
+                case "ヘアスタイル": return 1;
+                case "ドレス": return 2;
+                case "コート": return 3;
+                case "トップス": return 4;
+                case "ボトムス": return 5;
+                case "靴下": return 6;
+                case "靴下・ガーター": return 7;
+                case "シューズ": return 8;
+                case "アクセサリー": return 9;
+
+                case "ヘアアクセサリー": return 10;
+                case "ヘッドアクセ": return 11;
+                case "ヴェール": return 12;
+                case "カチューシャ": return 13;
+
+                case "つけ耳": return 14;
+                case "耳飾り": return 15;
+
+                case "首飾り": return 16;
+                case "マフラー": return 17;
+                case "ネックレス": return 18;
+
+                case "腕飾り": return 19;
+                case "右手飾り": return 20;
+                case "左手飾り": return 21;
+                case "手袋": return 22;
+
+                case "手持品": return 23;
+                case "右手持ち": return 24;
+                case "左手持ち": return 25;
+                case "両手持ち": return 26;
+
+                case "腰飾り": return 27;
+
+                case "特殊": return 28;
+                case "フェイス": return 29;
+                case "ボディ": return 30;
+                case "タトゥー": return 31;
+                case "羽根": return 32;
+                case "しっぽ": return 33;
+                case "前景": return 34;
+                case "後景": return 35;
+                case "吊り": return 36;
+                case "床": return 37;
+                case "肌": return 38;
+
+                case "メイク": return 39;
+            }
+
+            throw new Exception();
+        }
+
+        public static double GetRate(int kindId)
+        {
+            switch (kindId)
+            {
+                case 1: return 5;
+
+                case 2: return 20;
+
+                case 3: return 2;
+
+                case 4: return 10;
+
+                case 5: return 10;
+
+                case 6:
+                case 7: return 3;
+
+                case 8: return 4;
+
+                case 9:
+                case 10:
+                case 11:
+                case 12:
+                case 13:
+                case 14:
+                case 15:
+                case 16:
+                case 17:
+                case 18:
+                case 19:
+                case 20:
+                case 21:
+                case 22:
+                case 23:
+                case 24:
+                case 25:
+                case 26:
+                case 27:
+                case 28:
+                case 29:
+                case 30:
+                case 31:
+                case 32:
+                case 33:
+                case 34:
+                case 35:
+                case 36:
+                case 37:
+                case 38: return 2;
+
+                case 39: return 1;
+            }
+
+            throw new Exception();
         }
     }
 }
