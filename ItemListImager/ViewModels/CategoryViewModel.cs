e71dc21ef107ff1d196794588c20c39a685291c9
ShTair <git@vsh.jp>
2017-07-19 00:30:35 +0900

    リストの読み込みを実装

---
+++ ItemListImager/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace ItemListImager.ViewModels
+{
+    class CategoryViewModel
+    {
+        public int Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string ItemsStr { get; private set; }
+
+        public static CategoryViewModel Load(string path)
+        {
+            var vm = new CategoryViewModel();
+            var ns = Path.GetFileNameWithoutExtension(path).Split('_');
+            vm.Name = $"{ns[2]}-{ns[3]}";
+            vm.Id = int.Parse(ns[1]) * 10 + int.Parse(ns[2]);
+
+            var items = (from line in File.ReadLines(path)
+                         let sp = line.Split('\t')
+                         select $"{sp[3].Split(',')[0]}({sp[0]})").Take(10).ToList();
+
+            if (items.Count == 0) return null;
+
+            vm.ItemsStr = string.Join(", ", items);
+
+            return vm;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Name}: {ItemsStr}";
+        }
+    }
+}
---
+++ ItemListImager/ViewModels/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+
+namespace ItemListImager.ViewModels
+{
+    class MainViewModel
+    {
+        public ObservableCollection<CategoryViewModel> Categories { get; private set; }
+
+        public MainViewModel()
+        {
+            var path = Environment.GetCommandLineArgs()[1];
+            Categories = new ObservableCollection<CategoryViewModel>(Directory.EnumerateFiles(path, "*.txt").Select(file => CategoryViewModel.Load(file)).Where(t => t != null).OrderBy(t => t.Id));
+        }
+    }
+}
